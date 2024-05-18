6678238729a0e749439be09eda0f72c8984fdae7
MuttonString <1623492183@qq.com>
2024-05-18 13:06:27 +0800

    完成第18章：Django入门

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -22,7 +22,8 @@ class Entry(models.Model):
         '''返回一个表示条目的简单字符串'''
         # return f'{self.text[:50]}...'
         # 练习 18.2：简短的条目　当前，当 Django 在管理网站或 shell 中显示Entry 实例时，模型 Entry__str__() 方法都在其末尾加上省略号。请在 __str__() 方法中添加一条 if 语句，仅在条目长度超过 50 个字符时才添加省略号。使用管理网站添加一个不超过 50 个字符的条目，并核实在显示它时不带省略号。
-        if len(self.text) > 50:
-            return f'{self.text[:50]}...'
+        limit = 50
+        if len(self.text) > limit:
+            return f'{self.text[:limit]}...'
         else:
             return self.text
