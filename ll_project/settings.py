5e7037cc9797b700ac6dc6233186fedf9c567d4f
MuttonString <1623492183@qq.com>
2024-05-20 12:09:11 +0800

    关闭调试模式

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v!+3c$!*si@lhpafe88&x*i*c5pejqxis3=(swki3k+8jw)$)0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
